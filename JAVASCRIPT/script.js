867be8c4374d5dfb15f078fef60dc2e9037ca038
alexandreleao <erdleosalgueiro@gmail.com>
2022-02-08 09:22:20 -0300

    Primeiro commit

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,112 @@
+
+/* Formas de usar o javascript
+
+document.getElementById("titulo").innerHTML = "Opa, tudo bem?";
+
+document.write("Algum texto...");
+
+window.alert("Mensagem de exemplo!!!");
+
+console.log("A mensagem que eu inserir aqui aparecerá no console.");
+
+*/
+
+//Variáveis//
+//var nome = "Alexandre";
+//var idade = 90;
+
+//alert(nome);
+/*
+var x = 10;
+var y = 15;
+
+var total = x + y;
+
+
+alert(total);*/
+/*
+var nome = "Alexandre"; // String relacionado a texto ou palavra
+
+var sobrenome = "Salgueiro";
+
+var nomecompleto = nome+" "+sobrenome;
+
+alert(nomecompleto); */
+/*
+var x = "10";
+var y = "15";
+
+var total = x + y;
+
+alert(total); */
+
+/*
+var nome = "Alexandre";
+var idade = 90;
+
+var total = nome + " " + idade;
+
+alert(total); 
+*/
+/*
+var hora = 8;
+
+if( hora < 12 ) {
+    console.log( "Bom dia!" );
+} else {
+    console.log("Não é de manhã!");
+} */
+/*
+var hora = 18;
+/*
+if(hora < 12) {
+    console.log("Bom dia!");
+} else if(hora < 18)
+console.log("Boa tarde!");
+else if( hora <= 23){
+    console.log("Boa noite!")
+} 
+
+if( hora == 12 || hora == 18) {
+    console.log("Você está na hora do rush!");
+} */
+
+/*
+var nome = "Alexandre";
+
+//HOISTING -> Enviada para o escopo geral da tela.
+
+//Ex;
+
+console.log(window.nome);
+*/
+
+//let nome = "Alexandre";
+
+/*let nome = "Alexandre";
+
+if(nome == "Alexandre"){
+
+    let idade = 90;
+    console.log(idade);
+} */
+
+/*let nome = "João";
+ nome = "Pedro";
+
+console.log(nome); */
+/*
+const nome ="Alexandre";
+
+console.log(nome); */
+/*
+nome = "Alexandre";
+
+idade = 42;
+
+if(idade >= 42 && idade == 42){
+    console.log(nome);
+}
+*/
+
+document.getElementById('exemplo').innerHTML = "Pedro <button>Botão</button>";
---
+++ script.js
@@ -0,0 +1,10 @@
+//document.getElementById('...'); ->Pegue por id.-> Mais usado.
+//document.getElementsByClassName('...'); ->Pegue por nome da classe.
+//document.getElementsByTagName('...');->Pegue por nome da tag.
+//document.getElementsByName('...')->Pegue por name do campo input.->Menos usado.
+
+
+//document.getElementById('exemplo').innerHTML = "<b>Alexandre</b>";
+//document.getElementsByClassName('lista')[1].innerHTML = "Lista Editada com sucesso!";
+//document.getElementsByTagName('input');
+//document.getElementsByName('telefone');
