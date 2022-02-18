eef1fcb7858cd319ba1ce2e989aa1c78a525bf2a
alexandreleao <erdleosalgueiro@gmail.com>
2022-02-18 17:08:02 -0300

    Aula de Mudança de estilos

---
+++ Tutorial/teste.js
@@ -0,0 +1,16 @@
+const para = document.querySelector('p');
+
+para.addEventListener('click', atualizarNome);
+
+
+function atualizarNome(){
+var nome = prompt('Insira um novo nome');
+para.textContent = 'Jogador 1: ' + nome;
+}
+
+
+function criarParagrafo(){
+    let para = document.createElement('p');
+    para.textContent = 'Você clicou no botão!';
+    document.body.appendChild(para);
+}
