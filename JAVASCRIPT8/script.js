16df1fd3da4033869d34f60fa6f6fb3a180044c1
alexandreleao <erdleosalgueiro@gmail.com>
2022-02-21 17:06:13 -0300

    Array

---
+++ JAVASCRIPT8/script.js
@@ -0,0 +1,23 @@
+/*let carros = [
+'Fusca', 
+'Gol', 
+'Passat', 
+'Golf',
+function(){
+    console.log('Testando 1,2,3...');
+}
+];
+
+//console.log( carros[6] );
+
+carros[4]();
+*/
+
+let Agenda = [
+    ['João', 'José', 'Maria', 'Otavio', 'Fernanda', 'Gustavo'],
+    ['EriJonson', 'Esli', 'Ricardo', 'Daniela', 'Eduarda', 'Telma', 'Tereza'],
+    ['Nivaldo', 'Roberto', 'Valter', 'Marcos', 'Hebert', 'Cristino'],
+    ['Dida', 'Cafú', 'Ruan', 'Lúcio', 'R.Carlos', 'G.Silva', 'Zé-Roberto', 'Kaka', 'Ronaldinho','Ronaldo', 'Adriano']
+];
+
+console.log(Agenda);
